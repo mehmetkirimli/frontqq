b215197c7b83aa7c0c0d7b0525da26e7fd470678
mehmetkirimli <mehmet.kirimli@vbt.com.tr>
2024-01-16 17:51:23 +0300

    post components denendi

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Post from "./components/Post/Post";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Post />
   </React.StrictMode>
 );
 
