e9f22891585462a26c4a7fa68d5ec35f3faf9367
mehmetkirimli <mehmet.kirimli@vbt.com.tr>
2024-01-24 17:36:36 +0300

    install Chakra UI and upgrade post card

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
+import { ChakraProvider, extendBaseTheme, theme as chakraTheme } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+// ---------------------------------------------------------------------------------------------------------
+
+const { Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route exact path="/" Component={Home}></Route>
-          <Route exact path="/profiles/:profileId" Component={Profile}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ChakraProvider theme={theme}>
+      <div className="App">
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+            <Route exact path="/" Component={Home}></Route>
+            <Route exact path="/profiles/:profileId" Component={Profile}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ChakraProvider>
   );
 }
 
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,7 @@
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, IconButton, Text } from "@chakra-ui/react";
 import React from "react";
-// import "../Post/Post.scss";
+import { BiChat, BiLike, BiShare } from "react-icons/bi";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import "../Post/Post.css";
 
 function Post(props) {
@@ -7,9 +9,70 @@ function Post(props) {
 
   return (
     <div className="postContainer">
-      {title}
-      -------------
-      {text}
+      {/* ---------------------------------------------------------------------------------------------------------- */}
+      <Box
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        boxShadow="lg"
+        width="500px" /* İstediğiniz genişliği belirleyebilirsiniz */
+        margin="10px" /* İstediğiniz boşluk miktarını belirleyebilirsiniz */
+      >
+        <Card maxW="md">
+          <CardHeader flexDirection="column">
+            <Flex spacing="1">
+              <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+                {/* <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" /> */}
+
+                <Box>
+                  <Heading size="sm">Kullanıcı Adı Soyadı</Heading>
+                  <Text>@username</Text>
+                  <Text>{title}</Text>
+                </Box>
+              </Flex>
+              <IconButton variant="ghost" colorScheme="gray" aria-label="See menu" icon={<BsThreeDotsVertical />} />
+            </Flex>
+          </CardHeader>
+
+          {/* ---------------------------------------------------------------------------------------------------------- */}
+
+          <CardBody textAlign="center" justifyContent="center">
+            <Text alignItems="center">{text}</Text>
+          </CardBody>
+
+          {/* ---------------------------------------------------------------------------------------------------------- */}
+
+          {/* <Image
+          objectFit="cover"
+          src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          alt="Chakra UI"
+        /> */}
+
+          {/* ---------------------------------------------------------------------------------------------------------- */}
+
+          <CardFooter
+            justify="space-between"
+            flexWrap="wrap"
+            sx={{
+              "& > button": {
+                minW: "136px",
+              },
+            }}
+          >
+            <Button flex="1" variant="ghost" leftIcon={<BiLike />}>
+              Like
+            </Button>
+            <Button flex="1" variant="ghost" leftIcon={<BiChat />}>
+              Comment
+            </Button>
+            <Button flex="1" variant="ghost" leftIcon={<BiShare />}>
+              Share
+            </Button>
+          </CardFooter>
+
+          {/* ---------------------------------------------------------------------------------------------------------- */}
+        </Card>
+      </Box>
     </div>
   );
 }
